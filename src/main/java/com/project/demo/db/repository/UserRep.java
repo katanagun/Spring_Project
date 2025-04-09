14583048e02d98df3aa258ce9d71f0f9cc5d202b
Bogdan Vahrin <cormack567@mail.ru>
2025-04-10 00:04:37 +0300

    dont create tables

--- src/main/java/com/project/demo/db/repository/UserRep.java
+++
@@ -1,9 +0,0 @@
-package com.project.demo.db.repository;
-
-import com.project.demo.db.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRep extends JpaRepository<User, Long> {
-}
